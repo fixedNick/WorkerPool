0c7972eff16300422eec566e55972eede21d2e30
fixedNick <kkos44911@gmail.com>
2026-01-21 20:02:18 +0300

    Added module `Statistics`

---
+++ core/statistics.go
@@ -0,0 +1,45 @@
+package core
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+type StatisticsCollector interface {
+	Start()
+	TaskStart()
+	TaskFinished(success bool)
+
+	// Getters
+	AvgThroughput() float64 // Tasks per second
+	TasksTotal() int
+	TasksSucceed() int
+	TaskFailed() int
+	StartTime() time.Time
+}
+
+type Statistics struct {
+	startTime   time.Time
+	totalTasks  atomic.Int32
+	successfull atomic.Int32
+	failed      atomic.Int32
+}
+
+func (s *Statistics) Start()     { s.startTime = time.Now() }
+func (s *Statistics) TaskStart() { s.totalTasks.Add(1) }
+func (s *Statistics) TaskFinished(success bool) {
+	if success {
+		s.successfull.Add(1)
+	} else {
+		s.failed.Add(1)
+	}
+}
+
+// Getters
+func (s *Statistics) AvgThroughput() float64 {
+	return time.Since(s.startTime).Seconds() / float64(s.TasksTotal())
+}
+func (s *Statistics) TasksSucceed() int    { return int(s.successfull.Load()) }
+func (s *Statistics) TasksFailed() int     { return int(s.failed.Load()) }
+func (s *Statistics) TasksTotal() int      { return int(s.totalTasks.Load()) }
+func (s *Statistics) StartTime() time.Time { return s.startTime }
